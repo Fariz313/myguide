953a9d6b820fb997d1fe23f78b46228a7e31d4a5
prasetyo <tryprasetyoadi2420@gmail.com>
2024-01-09 19:10:20 +0700

    push

--- src/main/java/com/k1/myguide/Controllers/TransactionController.java
+++ src/main/java/com/k1/myguide/Controllers/TransactionController.java
@@ -1,6 +1,7 @@
 package com.k1.myguide.Controllers;
 
 import java.util.List;
+import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 import org.springframework.http.HttpStatus;
@@ -9,18 +10,24 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+import com.google.cloud.firestore.WriteResult;
+import com.google.firebase.cloud.FirestoreClient;
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.CollectionReference;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.Firestore;
 import com.k1.myguide.Models.Transaction;
 import com.k1.myguide.Models.User;
 import com.k1.myguide.Serivices.TransactionService;
 import com.k1.myguide.Serivices.UserService;
 import com.k1.myguide.Serivices.TransactionService;
 import com.k1.myguide.Utils.Response;
-
+import com.google.cloud.Timestamp;
 @RestController
 @RequestMapping(value = "transaction")
 @CrossOrigin("*")
@@ -55,5 +62,28 @@ public class TransactionController {
                 .status(HttpStatus.OK)
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(response);
+    }
+
+    @PostMapping("/save")
+    public Transaction saveTransaction(@RequestBody Transaction transaction) throws ExecutionException, InterruptedException {
+        try {
+            Firestore dbFirestore = FirestoreClient.getFirestore();
+            CollectionReference transactions = dbFirestore.collection("Transactions");
+
+            UUID uuid = UUID.randomUUID();
+            transaction.setId(uuid.toString());
+            transaction.setCreated_at(Timestamp.now());
+
+            DocumentReference documentReference = transactions.document(uuid.toString());
+            ApiFuture<WriteResult> writeResultApiFuture = documentReference.set(transaction);
+
+            // Tambahkan log atau penanganan lain jika diperlukan
+            System.out.println("Transaction saved successfully");
+
+            return transaction;
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw e;
+        }
     }
 }
